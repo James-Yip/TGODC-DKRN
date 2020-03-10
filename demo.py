a7e398b05342e7eceb3cbf821be95a958d29d792
James Yip <874244887@qq.com>
2020-03-11 00:07:11 +0800

    remove useless files

--- demo.py
+++
@@ -1,8 +0,0 @@
-from target_chat import target_chat_instance
-
-num_sessions = input('Please input the number of sessions you wanna try: ')
-for i in range(int(num_sessions)):
-    responses = []
-    target_chat_instance.chat()
-    while len(responses) < 2:
-        responses = target_chat_instance.chat(input('HUMAN: '))
